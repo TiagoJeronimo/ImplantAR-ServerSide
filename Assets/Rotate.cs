1c59c3013f5aa558d7306c5f5c23e29c4e9aa47a
Tiago Jerónimo Silva <tiago_j.s@hotmail.com>
2017-07-13 16:21:57 +0100

    Interface with Coronal, axial and sagital plane

---
+++ Assets/Rotate.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+
+    // rotate
+    public float rotSpeed = 4.0f;
+    bool isRotating;
+    Vector3 rotationAxisX;
+    Vector3 rotationAxisY;
+    Vector3 mouseOrigin;
+    Vector3 angleDelta;
+    GameObject rotationCentre;
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1)) {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit, 500.0f)) {
+                //Debug.Log("You selected the " + hit.transform.name);
+                isRotating = true;
+                if (Input.GetMouseButtonDown(0)) rotationCentre = this.gameObject;
+                if (Input.GetMouseButtonDown(1)) rotationCentre = Camera.main.gameObject;
+                mouseOrigin = Input.mousePosition;
+            }
+        }
+
+        if (isRotating) {
+            rotationAxisX = Camera.main.transform.up;
+            rotationAxisY = Camera.main.transform.right;
+            angleDelta = (Input.mousePosition - mouseOrigin) / Screen.width;
+            angleDelta *= rotSpeed;
+            angleDelta.x *= -1;
+            this.transform.RotateAround(rotationCentre.transform.position, rotationAxisX, angleDelta.x);
+            this.transform.RotateAround(rotationCentre.transform.position, rotationAxisY, angleDelta.y);
+            if (!Input.GetMouseButton(0) && !Input.GetMouseButton(1)) isRotating = false;
+        }
+    }
+}
---
+++ Assets/Script/SliceAlong.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliceAlong : MonoBehaviour {
+
+    public Slider mainSlider;
+    public Text NumberOfSlices;
+
+    //Invoked when a submit button is clicked.
+    public void SubmitSliderSetting() {
+        //Displays the value of the slider in the console.
+        foreach (Transform child in transform) {
+            child.gameObject.SetActive(false);
+        }
+        this.transform.GetChild((int)mainSlider.value).gameObject.SetActive(true);
+
+        //Debug.Log((int)mainSlider.value); //it's jumping some images
+    }
+
+    public void Update() {
+        NumberOfSlices.text = "S: " + (int) mainSlider.value;
+    }
+}
