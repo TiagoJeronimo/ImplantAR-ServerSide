e678c7faa0aa1431741833b89765b4bdbf3781c0
Tiago Jerónimo Silva <tiago_j.s@hotmail.com>
2017-07-18 11:12:01 +0100

    Rotation tweaks

--- Assets/Script/Rotate.cs
+++ Assets/Script/Rotate.cs
@@ -6,39 +6,10 @@ public class Rotate : MonoBehaviour {
 
     // rotate
     public float rotSpeed = 4.0f;
-    bool isRotating;
-    Vector3 rotationAxisX;
-    Vector3 rotationAxisY;
-    Vector3 mouseOrigin;
-    Vector3 angleDelta;
-    GameObject rotationCentre;
 
-    // Update is called once per frame
-    void Update () {
-        if (Input.GetMouseButtonDown(0)) {
-            RaycastHit hit;
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(ray, out hit, 500.0f)) {
-                //Debug.Log("You selected the " + hit.transform.name);
-                isRotating = true;
-                rotationCentre = this.gameObject;
-                mouseOrigin = Input.mousePosition;
-            }
-        }
-
-        if (isRotating) {
-            rotationAxisX = Camera.main.transform.up;
-            rotationAxisY = Camera.main.transform.right;
-            angleDelta = (Input.mousePosition - mouseOrigin) / Screen.width;
-            angleDelta *= rotSpeed;
-            angleDelta.x *= -1;
-            this.transform.RotateAround(rotationCentre.transform.position, rotationAxisX, angleDelta.x);
-            this.transform.RotateAround(rotationCentre.transform.position, rotationAxisY, angleDelta.y);
-
-            /*RotX -= angleDelta.x;
-            RotY += angleDelta.y;
-            transform.eulerAngles = new Vector3(RotY, 0, RotX);*/
-            if (!Input.GetMouseButton(0)) isRotating = false;
-        }
+    private void OnMouseDrag() {
+        float h = rotSpeed * Input.GetAxis("Mouse X");
+        float v = rotSpeed * Input.GetAxis("Mouse Y");
+        transform.Rotate(-v, 0, h);
     }
 }
--- Assets/Script/SliceAlong.cs
+++ Assets/Script/SliceAlong.cs
@@ -8,18 +8,27 @@ public class SliceAlong : MonoBehaviour {
     public Slider mainSlider;
     public Text NumberOfSlices;
 
+    private int SliceNumber;
+
     //Invoked when a submit button is clicked.
     public void SubmitSliderSetting() {
         //Displays the value of the slider in the console.
         foreach (Transform child in transform) {
             child.gameObject.SetActive(false);
         }
-        this.transform.GetChild((int)mainSlider.value).gameObject.SetActive(true);
+        this.transform.GetChild(SliceNumber).gameObject.SetActive(true);
 
         //Debug.Log((int)mainSlider.value); //it's jumping some images
     }
 
     public void Update() {
-        NumberOfSlices.text = "S: " + (int) mainSlider.value;
+        SliceNumber = (int)mainSlider.value;
+        
+        if (Input.GetAxis("Mouse ScrollWheel") != 0f) {      
+            SliceNumber += (int) Input.GetAxis("Mouse ScrollWheel");
+            Debug.Log("Scrolling");
+        }
+
+        NumberOfSlices.text = "S: " + SliceNumber;
     }
 }
