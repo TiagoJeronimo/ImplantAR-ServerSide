8d0fb416bf0ab328f0063bcc9ad1065ca8e44aab
Tiago Jerónimo Silva <tiago_j.s@hotmail.com>
2017-10-12 11:13:06 +0100

    preparing for zoom stuff

---
+++ Assets/Zoom.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zoom : MonoBehaviour {
+
+	public float ZoomValue = 10;
+	public float NormalValue = 60;
+	public float Smooth = 5;
+
+	private bool IsZoomed = false;
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (1)) {
+			IsZoomed = !IsZoomed;
+		}
+
+		if (IsZoomed) {
+			GetComponent<Camera> ().fieldOfView = Mathf.Lerp (GetComponent<Camera> ().fieldOfView, ZoomValue, Time.deltaTime * Smooth);
+		} else {
+			GetComponent<Camera> ().fieldOfView = Mathf.Lerp (GetComponent<Camera> ().fieldOfView, NormalValue, Time.deltaTime * Smooth);
+		}
+	}
+}
