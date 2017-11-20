e77070537485fa40a8813b6868dd4bcc5adab2dc
unknown <tiago_j.s@hotmail.com>
2017-11-20 11:16:47 +0000

    some tweaks

--- Assets/Script/ShowSlice.cs
+++ Assets/Script/ShowSlice.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class ShowSlice : MonoBehaviour {
 	//Check if the plan is touching the implant so it can proceed with the stencil process
-	void OnTriggerStay(Collider other) {
+	/*void OnTriggerStay(Collider other) {
 		GetComponent<Renderer>().enabled = true;
     }
 
 	void OnTriggerExit(Collider other) {
 		GetComponent<Renderer>().enabled = false;
-	}
+	}*/
 }
