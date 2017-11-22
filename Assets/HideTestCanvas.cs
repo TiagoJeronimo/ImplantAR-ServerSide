3e5c31c70214f231891abdf2906ad02ffacf3fdf
unknown <tiago_j.s@hotmail.com>
2017-11-22 16:33:45 +0000

    more test stuff (writing test results on files)

---
+++ Assets/HideTestCanvas.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideTestCanvas : MonoBehaviour {
+
+	public void HideCanvas() {
+		gameObject.SetActive(false);
+	}
+}
--- Assets/Script/Test.cs
+++ Assets/Script/Test.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Test : MonoBehaviour {
 
@@ -9,15 +10,37 @@ public class Test : MonoBehaviour {
 
 	public Transform Implant;
 	public bool DebugOn;
+	public GameObject TextObject;
+	private string Text;
+
+	private string Path;
+	private string TestVariables;
 
 	void Update () {
-		Vector3 auxPosition = new Vector3(transform.localPosition.x, transform.localPosition.y, transform.localPosition.z + 5.34f);
-		PositionDiscrepancy = Implant.transform.localPosition - auxPosition;
-		RotationDiscrepancy = Implant.transform.localEulerAngles - transform.localEulerAngles;
+		Text = TextObject.GetComponent<Text>().text;
+		Path = Application.dataPath + "/Results" + Text + ".txt";
+		if (Text != "") {
+			Vector3 auxPosition = new Vector3(transform.localPosition.x, transform.localPosition.y, transform.localPosition.z + 5.34f);
+			PositionDiscrepancy = Implant.transform.localPosition - auxPosition;
 
-		if (DebugOn) {
-			//Debug.Log ("PositionDiscrepancy: " + PositionDiscrepancy);
-			//Debug.Log ("RotationDiscrepancy: " + RotationDiscrepancy);
+			RotationDiscrepancy = Implant.transform.localEulerAngles - transform.localEulerAngles;
+
+			float angleX = RotationDiscrepancy.x + 90;
+			angleX = (angleX > 180) ? angleX - 360 : angleX;
+			float angleY = RotationDiscrepancy.y;
+			angleY = (angleY > 180) ? angleY - 360 : angleY;
+			float angleZ = RotationDiscrepancy.z;
+			angleZ = (angleZ > 180) ? angleZ - 360 : angleZ;
+
+			RotationDiscrepancy = new Vector3 (angleX,angleY,angleZ);
+
+			if (DebugOn) {
+				Debug.Log ("PositionDiscrepancy: " + PositionDiscrepancy.ToString());
+				Debug.Log ("RotationDiscrepancy: " + RotationDiscrepancy.ToString());
+			}
+
+			TestVariables = "User: " + Text + "\r\n" + "Position Discrepancy: " + PositionDiscrepancy.ToString () + "\r\n" + "Rotation Discrepancy: " + RotationDiscrepancy.ToString ();
+			System.IO.File.WriteAllText (Path, TestVariables);
 		}
 	}
 }
